30bc4e23e1796b35d29b4f62442a47fa37e5f8ed
Your Name <you@example.com>
2017-09-13 17:39:30 +0530

    s

--- quadratic.py
+++ quadratic.py
@@ -1,14 +1,9 @@
-global r1,r2
+global r1,r2,d
 import cmath
 def roots(a,b,c,d):
-    if(d==0):
-      r1=(-b)/(2*a)
-      r2=(-b)/(2*a)
-      return r1,r2 #print('the roots are equal and real \n r1={0} r2={1}'.format(r1,r2))
-    else:
       r1=(-b+cmath.sqrt(d))/2*a
-      r2=(-b-cmath.sqrt(d))/2*a
-      return r1,r2 #print('the roots are complex\n r1={0} r2={1}'.format(r1,r2)) 
+      r2=r1.conjugate()
+      return r1,r2 
 def read():
     return float(input()) 
 print('enter coefficients of quadratic equatin  from higer order')
