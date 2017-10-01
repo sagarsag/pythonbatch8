be6e61f6114d4aae28a9a957fe5870f7af66c9c6
sagarsag <sagvignesh@gmail.com>
2017-10-01 08:28:02 +0530

    S

--- dic.py
+++ dic.py
@@ -1,14 +1,12 @@
 def read():
   return int(input())
-def compute(text):
+def compute(text,n,d):
   for i in range(n):
-    #text=(readstring().split('='))
     d[text[0]]=text[1]
   return d
 n=read()
 d={}
-t=[]
 for i in range(n):
   text=list(input().split('='))
-  d=compute(text)
+  d=compute(text,n,d)
 print(d)
