e6c3c99216019a69866c6725e74ede45d958d07e
sagarsag <sagvignesh@gmail.com>
2017-10-06 12:06:59 +0530

    Add files via upload

--- dic.py
+++ dic.py
@@ -1,13 +1,13 @@
 def read():
   return int(input())
-def compute(text,n,d):
+def compute(text):
   for i in range(n):
-    t=(text.split('='))
+    t=text.split('=')
     d[t[0]]=t[1]
   return d
 n=read()
 d={}
 for i in range(n):
-  text=(input())
-  d=compute(text,n,d)
+  text=input()
+  d=compute(text)
 print(d)
